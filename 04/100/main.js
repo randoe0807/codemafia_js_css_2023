742ce3c3827c3e387924deb7ac6d92981cb91b16
Ryuichi Ando <130325925+randoe0807@users.noreply.github.com>
2023-12-14 13:02:37 +0000

    73. DOMContentLoadedと　Loadの違い

--- 04/100/main.js
+++ 04/100/main.js
@@ -1,10 +1,18 @@
-const dcl = document.querySelector('.dcl');
-const load = document.querySelector('.load');
+// const dcl = document.querySelector('.dcl');
+// const load = document.querySelector('.load');
 
-document.addEventListener("DOMContentLoaded", function () {
-    dcl.classList.add('done');
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//     dcl.classList.add('done');
+// });
 
-window.addEventListener("load", function () {
-    load.classList.add('done');
-});
+// window.addEventListener("load", function () {
+//     load.classList.add('done');
+// });
+
+const h1 = document.querySelector('h1');
+h1.style.color = 'red';
+
+// DOMContentLoadedとloadの違い
+// DOMContentLoadedはDOMツリーが構築されたら発火する
+// loadはページの読み込みが完了したら発火する
+// 読み込みが速いのはDOMContentLoaded
