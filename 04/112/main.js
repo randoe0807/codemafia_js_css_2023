6468f1843b5508cde0e0c76f033c0f03725a013c
Ryuichi Ando <130325925+randoe0807@users.noreply.github.com>
2023-12-15 16:16:25 +0000

    77 classとオブジェクト

--- 04/112/main.js
+++ 04/112/main.js
@@ -4,11 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const ta2 = new TextAnimation('.animate-title-2');
     ta.animate();
     ta2.animate();
-    // btn.addEventListener('click', ta.animate.bind(ta));
-    btn.addEventListener('click', function () {
-        ta.animate();
-        ta2.animate();
-    });
+    btn.addEventListener('click', ta.animate.bind(ta));
+    // btn.addEventListener('click', function () {
+    //     ta.animate();
+    //     ta2.animate();
+    // });
 });
 
 class TextAnimation {
