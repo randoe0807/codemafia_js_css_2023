f64b3d8e4aa33339856b9971b87c78fa285c0dac
Ryuichi Ando <130325925+randoe0807@users.noreply.github.com>
2023-12-17 14:33:45 +0000

    91 メニューの完成

--- 05/050/main.js
+++ 05/050/main.js
@@ -3,6 +3,7 @@ class MobileMenu{
     // 空のオブジェクトを作成
       this.DOM={};
       this.DOM.btn=document.querySelector('.mobile-menu__btn');
+      this.DOM.cover=document.querySelector('.mobile-menu__cover');
       this.DOM.container=document.querySelector('#global-container');
       this.eventType= this._getEventType();
       this._addEvent();
@@ -21,7 +22,9 @@ class MobileMenu{
         this.DOM.container.classList.toggle('menu-open');
       }
     _addEvent(){
-          this.DOM.btn.addEventListener(this.eventType,this._toggle.bind(this))
+          this.DOM.btn.addEventListener(this.eventType,this._toggle.bind(this));
+          this.DOM.cover.addEventListener(this.eventType,this._toggle.bind(this));
+
       }
 }
 new MobileMenu();
