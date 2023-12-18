7a0cbd80a06bdb0173f4a4a7693e0c4f63a80e55
Ryuichi Ando <130325925+randoe0807@users.noreply.github.com>
2023-12-18 02:48:42 +0000

    95 ヒーロースライダー実践編 part.1

--- 05/070/main.js
+++ 05/070/main.js
@@ -4,24 +4,4 @@ const swiper = new Swiper(".swiper", {
 //   direction: "vertical",
 //   loop: true,
 	effect: "coverflow",
-
-  // If we need pagination
-  pagination: {
-    // ページネーションの要素を指定
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    // 次のスライドへのbtn要素を指定
-    nextEl: ".swiper-button-next",
-    // 前のスライドへのbtn要素を指定
-    prevEl: ".swiper-button-prev",
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    // スクロールバーの要素を指定
-    el: ".swiper-scrollbar",
-  },
 });
