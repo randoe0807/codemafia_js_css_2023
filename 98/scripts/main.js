99ad7bdbe348257af444828aba771d2d0c6537c5
Ryuichi Ando <130325925+randoe0807@users.noreply.github.com>
2023-12-18 09:20:42 +0000

    109 ヒーロースライダーの読み込み

---
+++ 98/scripts/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hero = new HeroSlider(".swiper");
+    hero.start();
+    // hero.start({delay: 2000});
+  
+    // setTimeout(() => {
+    //   hero.stop();
+    // }, 5000)
+  });
