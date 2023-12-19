0b82f9da4624adbe5609386cdd506ae4a581c0f7
Ryuichi Ando <130325925+randoe0807@users.noreply.github.com>
2023-12-19 00:54:08 +0000

    113 スライドアニメーションを導入

--- 98/scripts/main.js
+++ 98/scripts/main.js
@@ -2,11 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const hero = new HeroSlider(".swiper");
     hero.start();
 
-    const cb  = function(el, isIntersecting) {
-      if(isIntersecting){
+    const _textAnimation  = function(el, inview) {
+      if(inview){
         const ta = new TweenTextAnimation(el);
         ta.animate();
       }
     }
-    const so = new ScrollObserver('.tween-animate-title', cb);
+    const so = new ScrollObserver('.tween-animate-title', _textAnimation);
+
+    const _inviewAnimation  = function(el, inview) {
+      if(inview){
+        el.classList.add('inview');
+      } else {
+        el.classList.remove('inview');
+      }
+    }
+    const so2 = new ScrollObserver('.cover-slide', _inviewAnimation);
   });
