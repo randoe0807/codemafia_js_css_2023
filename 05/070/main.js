73fa1c4f7e6453c8d7060f9a770c28be218b5ce9
Ryuichi Ando <130325925+randoe0807@users.noreply.github.com>
2023-12-20 05:36:57 +0000

    deploy

--- 05/070/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hero = new HeroSlider(".swiper");
-  hero.start({delay: 2000});
-
-  setTimeout(() => {
-    hero.stop();
-  }, 5000)
-});
-
